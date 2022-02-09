5ed93e8d156561614c39cf14b7f583edf4931a0c
thadchet <sogood_boss@hotmail.com>
2022-02-09 23:02:57 +0700

    d

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Register />} />
         <Route path="/test" element={<Register />} />
         <Route path="/web" element={<Register />} />
+        <Route path="/test/web" element={<Register />} />
         <Route path="/inventory" element={<Inventory />} />
       </Routes>
     </BrowserRouter>
